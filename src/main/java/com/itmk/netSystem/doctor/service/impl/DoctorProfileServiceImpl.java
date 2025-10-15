56d84b665d63a7ad155039b640dec13e5ecc3ddf
贾鹏 <2307289009@qq.com>
2025-10-15 17:43:45 +0800

    医生个人主页修改后端接口

---
+++ src/main/java/com/itmk/netSystem/doctor/service/impl/DoctorProfileServiceImpl.java
@@ -0,0 +1,93 @@
+package com.itmk.netSystem.doctor.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.github.yulichang.wrapper.MPJLambdaWrapper;
+import com.itmk.netSystem.teamDepartment.entity.Department;
+import com.itmk.netSystem.doctor.entity.DoctorProfileVo;
+import com.itmk.netSystem.doctor.mapper.DoctorProfileMapper;
+import com.itmk.netSystem.doctor.service.DoctorProfileService;
+import com.itmk.netSystem.userWeb.entity.SysUser;
+import com.itmk.netSystem.userWeb.service.userWebService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 医生个人主页服务实现
+ */
+@Service
+public class DoctorProfileServiceImpl extends ServiceImpl<DoctorProfileMapper, DoctorProfileVo> implements DoctorProfileService {
+
+    @Autowired
+    private userWebService userWebService;
+
+    @Override
+    public DoctorProfileVo getDoctorProfile(Long doctorId) {
+        MPJLambdaWrapper<SysUser> query = new MPJLambdaWrapper<>();
+        query.selectAll(SysUser.class)
+                .select(Department::getDeptName)
+                .leftJoin(Department.class, Department::getDeptId, SysUser::getDeptId)
+                .eq(SysUser::getUserId, doctorId);
+
+        SysUser sysUser = userWebService.getOne(query);
+        if (sysUser == null) {
+            return null;
+        }
+
+        DoctorProfileVo doctorProfileVo = new DoctorProfileVo();
+        BeanUtils.copyProperties(sysUser, doctorProfileVo);
+        return doctorProfileVo;
+    }
+
+    @Override
+    @Transactional
+    public boolean updateDoctorProfile(DoctorProfileVo doctorProfileVo) {
+        SysUser sysUser = new SysUser();
+        BeanUtils.copyProperties(doctorProfileVo, sysUser);
+        return userWebService.updateById(sysUser);
+    }
+
+    @Override
+    public DoctorProfileVo getMyProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        
+        QueryWrapper<SysUser> query = new QueryWrapper<>();
+        query.lambda().eq(SysUser::getUsername, username);
+        SysUser sysUser = userWebService.getOne(query);
+        
+        if (sysUser == null) {
+            return null;
+        }
+        
+        return getDoctorProfile(sysUser.getUserId());
+    }
+
+    @Override
+    @Transactional
+    public boolean updateMyProfile(DoctorProfileVo doctorProfileVo) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        
+        QueryWrapper<SysUser> query = new QueryWrapper<>();
+        query.lambda().eq(SysUser::getUsername, username);
+        SysUser currentUser = userWebService.getOne(query);
+        
+        if (currentUser == null) {
+            return false;
+        }
+        
+        // 只能更新自己的信息
+        if (!currentUser.getUserId().equals(doctorProfileVo.getUserId())) {
+            return false;
+        }
+        
+        SysUser sysUser = new SysUser();
+        BeanUtils.copyProperties(doctorProfileVo, sysUser);
+        return userWebService.updateById(sysUser);
+    }
+}
